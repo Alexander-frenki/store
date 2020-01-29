ddd767b1f5755f9cee5dbebc1d26cd691190fa9a
oleksandr.raspopov <alexander.frenkii@gmail.com>
2020-01-29 17:44:36 +0200

    iphone xr data

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 import { useParams, useLocation, useHistory } from "react-router-dom";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
@@ -13,6 +13,7 @@ function Product({ content, addToCart }) {
   let history = useHistory();
   let { productGroup, productHandle } = useParams();
   let variantID = query.get("variant");
+
   let product = content[productGroup].products[productHandle];
 
   let [currentVariant, setCurrentVariant] = useState(
@@ -23,6 +24,8 @@ function Product({ content, addToCart }) {
     color: currentVariant.color,
     capacity: currentVariant.capacity,
   });
+
+  let [isAnimate, setIsAnimate] = useState(false);
 
   useEffect(() => {
     history.push(`${window.location.pathname}?variant=${currentVariant.id}`);
@@ -44,6 +47,7 @@ function Product({ content, addToCart }) {
   function handleSubmit(e) {
     e.preventDefault();
     addToCart(currentVariant.id);
+    setIsAnimate(true);
   }
 
   return (
@@ -54,9 +58,15 @@ function Product({ content, addToCart }) {
             <h1 className="page_title">{product.name}</h1>
           </div>
         </div>
-        <div className="grid_row">
+        <div className="grid_row line">
           <div className="grid_desktop_6 product_images">
             <img src={currentVariant.image} alt={currentVariant.name} />
+            <AnimateImage
+              image={currentVariant.image}
+              alt={currentVariant.name}
+              isAnimate={isAnimate}
+              setIsAnimate={setIsAnimate}
+            />
           </div>
           <div className="grid_desktop_6 product_info">
             <form onSubmit={handleSubmit}>
@@ -101,14 +111,99 @@ function Product({ content, addToCart }) {
                   </label>
                 ))}
               </div>
-              <button type="submit" className="btn">
+              <button disabled={isAnimate} type="submit" className="btn">
                 SUBMIT
               </button>
             </form>
           </div>
         </div>
+        <div className=" product_description">
+          <div className="grid_row">
+            <div className="grid_desktop_12">
+              <h3>Description</h3>
+            </div>
+          </div>
+          <div className="grid_row">
+            {Object.keys(product.description).map(item => {
+              return (
+                <div
+                  className="product_description_item grid_desktop_6"
+                  key={item}
+                >
+                  <h5>{item}</h5>
+                  <p
+                    dangerouslySetInnerHTML={{
+                      __html: product.description[item],
+                    }}
+                  ></p>
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </div>
     </section>
+  );
+}
+
+let initialStateImage = {
+  width: "100%",
+  transform: "translate3d(0,0,0)",
+  opacity: 1,
+};
+
+function AnimateImage({ image, alt, isAnimate, setIsAnimate }) {
+  let animImage = useRef(null);
+  let [imageStyle, setImageStyle] = useState(initialStateImage);
+
+  let animateImage = useCallback(() => {
+    setImageStyle(imageStyle => ({
+      ...imageStyle,
+      width: "50px",
+      transition: "all 0.5s",
+    }));
+    setTimeout(() => {
+      let cartRect = document.querySelector(".cart").getBoundingClientRect();
+      let imgRect = animImage.current.getBoundingClientRect();
+      let topOffset =
+        cartRect.top < 0
+          ? -cartRect.top + imgRect.top
+          : cartRect.top + imgRect.top;
+      setImageStyle(imageStyle => ({
+        ...imageStyle,
+        transform: `translate3d(${cartRect.left - 100}px, ${-topOffset}px, 0)`,
+      }));
+      setTimeout(() => {
+        setImageStyle(imageStyle => ({
+          ...imageStyle,
+          width: "0px",
+          opacity: 0,
+        }));
+        setTimeout(() => {
+          setImageStyle(() => ({
+            ...initialStateImage,
+            transition: "all 0s",
+          }));
+          setIsAnimate(false);
+        }, 200);
+      }, 500);
+    }, 500);
+  }, [setIsAnimate]);
+
+  useEffect(() => {
+    if (isAnimate) {
+      animateImage();
+    }
+  }, [isAnimate, animateImage]);
+
+  return (
+    <img
+      style={{ ...imageStyle }}
+      className="product_images_animate"
+      src={image}
+      alt={alt}
+      ref={animImage}
+    />
   );
 }
 
