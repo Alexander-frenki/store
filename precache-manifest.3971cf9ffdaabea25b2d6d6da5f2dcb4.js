2dddbc3764d113950cc2ed75f311da4890a9cbee
oleksandr.raspopov <alexander.frenkii@gmail.com>
2020-02-03 13:58:16 +0200

    Updates

---
+++ precache-manifest.3971cf9ffdaabea25b2d6d6da5f2dcb4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f53e0cb94f5b1986868b849b823e466",
+    "url": "/store/index.html"
+  },
+  {
+    "revision": "9edbe073348323ced073",
+    "url": "/store/static/css/main.7fbaf056.chunk.css"
+  },
+  {
+    "revision": "94dd955ee0b8c843f586",
+    "url": "/store/static/js/2.5abb9a56.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/store/static/js/2.5abb9a56.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9edbe073348323ced073",
+    "url": "/store/static/js/main.edcb5122.chunk.js"
+  },
+  {
+    "revision": "f84de9975fba0e406c1b",
+    "url": "/store/static/js/runtime-main.51f1e728.js"
+  },
+  {
+    "revision": "9f061635f8ab4d9816c50bd243ea1653",
+    "url": "/store/static/media/logo.9f061635.svg"
+  }
+]);
