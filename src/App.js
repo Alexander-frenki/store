8a419252d25d73637e319b0b5261b3c436bb209b
oleksandr.raspopov <alexander.frenkii@gmail.com>
2020-02-03 09:47:58 +0200

    mac data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Header from "./components/Header";
 import Content from "./components/Content";
 import "./assets/style/main.scss";
+import { useHistory } from "react-router-dom";
 
 function App() {
   return (
@@ -10,9 +11,20 @@ function App() {
       <Router>
         <Header />
         <Content />
+        <Scroll />
       </Router>
     </>
   );
+}
+
+function Scroll() {
+  let history = useHistory();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [history.location.pathname]);
+
+  return null;
 }
 
 export default App;
